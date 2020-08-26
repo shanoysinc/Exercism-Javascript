80a047a80696b41f0cf738281fe4602b779a92d8
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2020-08-26 17:50:31 -0500

    first commit

---
+++ resistor-color/resistor-color.js
@@ -0,0 +1,23 @@
+//
+// This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const colorCode = (color) => {
+	const colors = [
+		"black",
+		"brown",
+		"red",
+		"orange",
+		"yellow",
+		"green",
+		"blue",
+		"violet",
+		"grey",
+		"white",
+	];
+	if (!color) return colors;
+	return colors.indexOf(color);
+};
+
+export const COLORS = undefined;
